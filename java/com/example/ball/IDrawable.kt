2bd86294385c08e9b3ca54eb14dbdab1f2f53554
Dubrovskiy-Konstantin <kd.5556600@gmail.com>
2021-11-10 16:11:02 +0300

    Deleted old files

--- java/com/example/ball/IDrawable.kt
+++
@@ -1,9 +0,0 @@
-package com.example.draw
-
-import android.graphics.Canvas
-import android.graphics.RectF
-
-interface IDrawable{
-    fun draw(canvas: Canvas)
-    fun contactPlayer(player: Player): Boolean
-}
--- java/com/example/ball/IMovable.kt
+++
@@ -1,5 +0,0 @@
-package com.example.draw
-
-interface IMovable: IDrawable {
-    fun move(max_X: Float, max_Y: Float)
-}
